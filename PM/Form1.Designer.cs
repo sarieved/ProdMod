7f0d7afba7acb7b524a144ff4059e10050c45f93
sarieved <sarieved@gmail.com>
2018-01-11 21:07:03 +0300

    Prod Model

---
+++ PM/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace PM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьБазуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьВсёToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(702, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьБазуToolStripMenuItem,
+            this.очиститьВсёToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // загрузитьБазуToolStripMenuItem
+            // 
+            this.загрузитьБазуToolStripMenuItem.Name = "загрузитьБазуToolStripMenuItem";
+            this.загрузитьБазуToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.загрузитьБазуToolStripMenuItem.Text = "Загрузить базу";
+            this.загрузитьБазуToolStripMenuItem.Click += new System.EventHandler(this.загрузитьБазуToolStripMenuItem_Click);
+            // 
+            // очиститьВсёToolStripMenuItem
+            // 
+            this.очиститьВсёToolStripMenuItem.Name = "очиститьВсёToolStripMenuItem";
+            this.очиститьВсёToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.очиститьВсёToolStripMenuItem.Text = "Очистить всё";
+            this.очиститьВсёToolStripMenuItem.Click += new System.EventHandler(this.очиститьВсёToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 251);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Факты:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(231, 44);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(459, 251);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Правила:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 357);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(148, 108);
+            this.listBox3.TabIndex = 5;
+            this.listBox3.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox3_MouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 341);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Выбранные факты:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Прямой вывод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(531, 357);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(146, 108);
+            this.listBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Результат:";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(190, 357);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(311, 108);
+            this.listBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Использованные правила:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Коэффициент:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 310);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 540);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Продукционная модель";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьБазуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьВсёToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ PM/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+
+namespace PM
+{
+    public partial class Form1 : Form
+    {
+        List<Fact> facts;
+        List<Fact> trueFacts;
+        List<Rule> rules;
+        public class Fact
+        {
+            public string ID;
+            public string inform;
+            public double coeff;
+            public Fact()
+            {
+
+            }
+            public Fact(Fact f)
+            {
+                ID = f.ID;
+                inform = f.inform;
+                coeff = 0;
+            }
+            public Fact(string id, string inf)
+            {
+                ID = id;
+                inform = inf;
+                coeff = 0;
+            }
+            public override string ToString()
+            {
+                return ID + ": " + coeff.ToString() + " " + inform;
+            }
+            public void set_coeff(double c)
+            {
+                coeff = c;
+            }
+        }
+
+        public class Rule
+        {
+            public string ID;
+            public List<string> left;
+            public string right;
+            public string inform;
+            public int check;
+            public double coeff;
+            public double min;
+            public Rule()
+            {
+                check = 0;
+            }
+            public Rule(string id, List<string> l, string r, string inf, string coef)
+            {
+                ID = id;
+                left = l;
+                right = r;
+                inform = inf;
+                check = 0;
+                coeff = Convert.ToDouble(coef);
+                min = double.MaxValue;
+            }
+
+            public Rule(Rule r2)
+            {
+                ID = r2.ID;
+                left = r2.left;
+                right = r2.right;
+                inform = r2.inform;
+                check = r2.check;
+                coeff = r2.coeff;
+                min = r2.min;
+            }
+            public override string ToString()
+            {
+                string left_part = "";
+                for (int i = 0; i < left.Count; i++)
+                {
+                    left_part += left[i] + " ";
+                }
+                return ID + ":|c =" + coeff.ToString() + "| " + left_part + "-->" + right + " " + inform;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void загрузитьБазуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string fname = openFileDialog1.FileName;
+            facts = new List<Fact>();
+            rules = new List<Rule>();
+            using (StreamReader reader = new StreamReader(File.Open(fname, FileMode.Open)))
+            {
+                string text;
+
+                while ((text = reader.ReadLine()) != null)
+                {
+                    string[] bits = text.Split(';');
+                    if (bits[0][0] == 'f')
+                    {
+                        facts.Add(new Fact(bits[0], bits[1]));
+                        listBox1.Items.Add(new Fact(bits[0], bits[1]));
+                    }
+                    else
+                    {
+                        string[] left = bits[1].Split(',');
+                        List<string> l = new List<string>();
+                        for (int i = 0; i < left.Length; i++)
+                            l.Add(left[i]);
+                        rules.Add(new Rule(bits[0], l, bits[2], bits[3], bits[4]));
+                        listBox2.Items.Add(new Rule(bits[0], l, bits[2], bits[3], bits[4]));
+                    }
+                }
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Fact F = new Fact((Fact)listBox1.SelectedItem);
+            F.set_coeff(Convert.ToDouble((numericUpDown1.Value)));
+            listBox3.Items.Add(F);
+        }
+
+        public int ind_find(List<Fact> l, string id)
+        {
+            int ind = -1;
+            for (int i = 0; i < l.Count; ++i)
+            {
+                if (l[i].ID == id)
+                {
+                    ind = i;
+                    break;
+                }
+            }
+            return ind;
+        }
+
+        public int ind_lb_find(string id)
+        {
+            int res = -1;
+            for (int i = 0; i < listBox4.Items.Count; ++i)
+                if (((Fact)listBox4.Items[i]).ID == id)
+                {
+                    res = i;
+                    break;
+                }
+            return res;
+        }
+
+        public double coef_c(double c1, double c2)
+        {
+            double res = 0;
+            if (c1 > 0 && c2 > 0)
+                res = c1 + c2 * (1 - c1);
+            else if (c1 < 0 && c2 < 0)
+                res = c1 + c2 * (1 + c1);
+            else
+                res = (c1 + c2) / (1 - Math.Min(Math.Abs(c1), Math.Abs(c2)));
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trueFacts = new List<Fact>();
+
+            for (int i = 0; i < listBox3.Items.Count; i++)
+            {
+                trueFacts.Add((Fact)listBox3.Items[i]);
+            }
+
+            bool stop = false;
+
+            while (!stop)
+            {
+                stop = true;
+                List<int> indToAdd = new List<int>();
+                for (int i = 0; i < trueFacts.Count; ++i)
+                {
+                    for (int j = 0; j < rules.Count; ++j)
+                    {
+                        if (trueFacts[i].ID.Equals(rules[j].right))
+                        {
+                            rules[j].check = -1;
+                            continue;
+                        }
+                        int ind = rules[j].left.IndexOf(trueFacts[i].ID);
+                        if (ind == -1)
+                            continue;
+                        else
+                        {
+                            rules[j].check += 1;
+                            rules[j].min = Math.Min(rules[j].min, trueFacts[i].coeff);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < rules.Count; ++i)
+                {
+                    if (rules[i].check == rules[i].left.Count)
+                    {
+                        int ind = ind_find(facts, rules[i].right);
+                        listBox5.Items.Add(rules[i].ToString());
+                        Fact f = new Fact(facts[ind]);
+                        f.set_coeff(rules[i].min * rules[i].coeff);
+                        if (ind_find(trueFacts, f.ID) == -1)
+                        {
+                            trueFacts.Add(f);
+                            listBox4.Items.Add(f);
+                        }
+                        else
+                        {
+                            int lb_ind = ind_lb_find(f.ID);
+                            listBox4.Items.RemoveAt(lb_ind);
+                            int tfacts_ind = ind_find(trueFacts, f.ID);
+                            double new_coeff = coef_c(trueFacts[tfacts_ind].coeff, f.coeff);
+                            trueFacts[tfacts_ind].set_coeff(new_coeff);
+                            f.set_coeff(new_coeff);
+                            listBox4.Items.Add(f);
+                        }
+                        stop = false;
+                    }
+                    rules[i].check = 0;
+                    rules[i].min = double.MaxValue;
+                }
+            }
+        }
+
+        private void listBox3_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listBox3.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                listBox3.Items.RemoveAt(index);
+            }
+        }
+
+        private void очиститьВсёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trueFacts = new List<Fact>();
+            int c1 = listBox3.Items.Count;
+            int c2 = listBox4.Items.Count;
+            int c3 = listBox5.Items.Count;
+
+            for (int i = 0; i < c1; i++)
+            {
+                listBox3.Items.RemoveAt(0);
+            }
+            for (int i = 0; i < c2; i++)
+            {
+                listBox4.Items.RemoveAt(0);
+            }
+            for (int i = 0; i < c3; i++)
+            {
+                listBox5.Items.RemoveAt(0);
+            }
+        }
+    }
+}
